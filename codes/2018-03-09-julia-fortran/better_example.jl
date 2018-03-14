a9e50f45a22fdddde3454d0ae958b792ea2b095f
c.dacostaf+github@gmail.com <c.dacostaf+github@gmail.com>
2018-03-14 20:31:10 +0000

    Remove old code

--- codes/2018-03-09-julia-fortran/better_example.jl
+++
@@ -1,9 +0,0 @@
-x = Cdouble[1,2,3,4]
-y = Cdouble[1,1,1,1]
-n = Cint[4]
-a = Cdouble[NaN]
-ccall((:better_dot, "./better_example.so"),
-      Void,
-      (Ref{Cint}, Ref{Cdouble}, Ref{Cdouble}, Ref{Cdouble}),
-      n, x, y, a)
-println(a)
